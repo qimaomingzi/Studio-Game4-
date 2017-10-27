798ee105d4aabd713c6576b19b3fea6a254ce975
Unknown <kz1065@nyu.edu>
2017-10-26 22:23:31 -0400

    Continue working on the ball control

--- Assets/PlayerTwoControl.cs
+++ Assets/PlayerTwoControl.cs
@@ -13,20 +13,21 @@ public class PlayerTwoControl : MonoBehaviour {
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
 	}
-	
+
 	// Update is called once per frame
 	void FixedUpdate () {
 		float turn = Input.GetAxis("Horizontal");
-		rb2d.AddTorque(torque * turn);
-		ForceX = Mathf.Sin (-transform.localRotation.z) / AngelToRadian;
-		ForceY = Mathf.Cos (-transform.localRotation.z) / AngelToRadian;
-
+		rb2d.AddTorque(-torque * turn);
+		ForceX = Mathf.Cos (transform.localEulerAngles);
+		ForceY = Mathf.Sin (transform.localRotation.z*Mathf.PI);
+		Debug.Log (ForceX + "   " + transform.localRotation.z*Mathf.PI);
 
 		if (Input.GetAxis("Vertical")> 0) {
 			rb2d.AddForce (new Vector2 (ForceX, ForceY)* 10);
 		}
 		if (Input.GetAxis ("Vertical") < 0) {
 			rb2d.angularVelocity = 0;
+			rb2d.velocity = Vector2.zero;
 		}
 	}
 }
