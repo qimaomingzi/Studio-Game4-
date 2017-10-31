5d3b247ea1602f693157704351a10b0fa0bd03ac
Unknown <kz1065@nyu.edu>
2017-10-31 15:09:47 -0400

    Tuning P2 control and try to add wall bounce

--- Assets/Scripts/PlayerOneControl.cs
+++ Assets/Scripts/PlayerOneControl.cs
@@ -5,40 +5,49 @@ using UnityEngine;
 public class PlayerOneControl : MonoBehaviour {
 	Rigidbody2D[] rb;
 	SpriteRenderer[] sr;
+	GameObject P2;
 	bool isLeft = true;
 	// Use this for initialization
 	void Start () {
 		rb = GetComponentsInChildren<Rigidbody2D> ();
 		sr = GetComponentsInChildren<SpriteRenderer> ();
-		sr [0].color = Color.gray;
+		sr [1].color = Color.gray;
+		P2 = GameObject.Find ("Player2");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (isLeft) {
 			if (Input.GetAxis ("Stick") > 0) {
-				rb [0].velocity = new Vector2 (0, 5);
+				rb [0].AddForce (Vector2.up * 200);
+				rb [0].velocity = new Vector2(0,Mathf.Min (rb [0].velocity.y, 20));
 			} else if (Input.GetAxis ("Stick") < 0){
-				rb [0].velocity = new Vector2 (0, -5);
+				rb [0].AddForce (Vector2.down * 200);
+				rb [0].velocity = new Vector2 (0, Mathf.Max (rb [0].velocity.y, -20));
 			}
 		}
 		if (!isLeft) {
 			if (Input.GetAxis ("Stick") > 0) {
-				rb [1].velocity = new Vector2 (0, 5);
+				rb [1].AddForce (Vector2.up * 200);
+				rb [1].velocity = new Vector2(0,Mathf.Min (rb [1].velocity.y, 20));
 			} else if (Input.GetAxis ("Stick") < 0){
-				rb [1].velocity = new Vector2 (0, -5);
+				rb [1].AddForce (Vector2.down * 200);
+				rb [1].velocity = new Vector2 (0, Mathf.Max (rb [1].velocity.y, -20));
 			}
 		}
 		if (Input.GetKeyDown ("a")) {
 			isLeft = true;
-			sr [0].color = Color.gray;
-			sr [1].color = Color.white;
+			sr [1].color = Color.gray;
+			sr [0].color = Color.white;
+			rb [1].velocity *= 0.2f;
+
 		}
 		if (Input.GetKeyDown ("d")) {
 			isLeft = false;
-			sr [1].color = Color.gray;
-			sr [0].color = Color.white;
+			sr [0].color = Color.gray;
+			sr [1].color = Color.white;
+			rb [0].velocity *= 0.2f;
 		}
-
 	}
+
 }
--- Assets/Scripts/PlayerTwoControl.cs
+++ Assets/Scripts/PlayerTwoControl.cs
@@ -22,14 +22,13 @@ public class PlayerTwoControl : MonoBehaviour {
 		} else if (rb2d.angularVelocity > -1000 && turn > 0) {
 			rb2d.AddTorque (-torque * turn);
 		}
-		Debug.Log (rb2d.angularVelocity);
 
 		if (Input.GetAxis("Vertical")> 0 && Mathf.Sqrt(rb2d.velocity.x*rb2d.velocity.x + rb2d.velocity.y *rb2d.velocity.y)<=20) {
 			rb2d.angularVelocity = 0;
 			rb2d.AddRelativeForce (Vector2.right* 10);
 		}
 		if (Input.GetKeyDown("down")) {
-			rb2d.velocity = new Vector2(rb2d.velocity.x/3,rb2d.velocity.y/3);
+			rb2d.velocity = new Vector2(rb2d.velocity.x/2,rb2d.velocity.y/2);
 		}
 	}
 }
