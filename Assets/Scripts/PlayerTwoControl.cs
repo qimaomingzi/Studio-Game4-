c8d72017421246d6d5d9758e28ab6c135119514c
Unknown <kz1065@nyu.edu>
2017-11-02 18:52:09 -0400

    Working on the second skill of P2

--- Assets/Scripts/PlayerTwoControl.cs
+++ Assets/Scripts/PlayerTwoControl.cs
@@ -6,12 +6,12 @@ public class PlayerTwoControl : MonoBehaviour {
 	Rigidbody2D rb2d;
 	public float torque;
 	public float MaxSpeed;
-	float ForceX;
-	float ForceY;
-	const float AngelToRadian= 180/Mathf.PI;
+	int STR = 100;
 	float SkillTime = 0;
+	float SkillTime2 = 0;
 	float Cooldown = 5;
 	bool BulletTime = false;
+	bool Shake = false;
 	// Use this for initialization
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
@@ -29,7 +29,7 @@ public class PlayerTwoControl : MonoBehaviour {
 		speed = Mathf.Sqrt (rb2d.velocity.x * rb2d.velocity.x + rb2d.velocity.y * rb2d.velocity.y);
 		if (Input.GetAxis("Vertical")> 0) {
 			rb2d.angularVelocity = 0;
-			rb2d.AddRelativeForce (Vector2.right* 100);
+			rb2d.AddRelativeForce (Vector2.right* STR);
 
 		}
 		if (Input.GetKeyDown("down")) {
@@ -41,6 +41,8 @@ public class PlayerTwoControl : MonoBehaviour {
 		if (Input.GetButtonDown ("P2Skill1") && Cooldown == 5) {
 			BulletTime = true;
 			SkillTime = 0;
+			torque *= 3;
+			STR *= 3;
 		}
 		if (BulletTime) {
 			Time.timeScale = 0.3f;
@@ -50,8 +52,24 @@ public class PlayerTwoControl : MonoBehaviour {
 				Time.timeScale = 1;
 				BulletTime = false;
 				Cooldown = 0;
+				torque /= 3;
+				STR /= 3;
 			}
 		}
+
+		if (Input.GetButtonDown ("P2Skill2") && Cooldown == 5) {
+			Shake = true;
+			SkillTime2 = 0;
+		}
+		if (Shake) {
+			transform.position = new Vector3(transform.position.x+Random.value/5-0.1f,transform.position.y+Random.value/5-0.1f,0);
+			SkillTime2 += Time.deltaTime;
+			if (SkillTime2 >= 1) {
+				Shake = false;
+			}
+		}
+
 		Cooldown = Mathf.Min (5, Cooldown += Time.deltaTime);
 	}
+
 }
